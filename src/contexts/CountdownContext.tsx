0d20c279d75ac434ec6b977e55b3844039331587
LeandroGonçalves <lezinhomococa@hotmail.com>
2021-03-31 19:25:55 -0300

    fix time

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -22,7 +22,7 @@ export function CountdownProvider({ children }: CountdownProviderProps) {
 
   const { startNewChallenge } = useContext(ChallengesContext);
 
-  const [time, setTime] = useState(0.05 * 60);
+  const [time, setTime] = useState(60 * 1);
   const [isActive, setIsActive] = useState(false);
   const [hasFinish, setHasFinished] = useState(false);
 
@@ -36,7 +36,7 @@ export function CountdownProvider({ children }: CountdownProviderProps) {
   function resetCountdown() {
     clearTimeout(countdownTimeout);
     setIsActive(false);
-    setTime(0.05 * 60);
+    setTime(60 * 1);
     setHasFinished(false);
   }
 
